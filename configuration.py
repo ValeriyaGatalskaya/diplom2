12b661ffe58c9d04fc999d93aba698f8e26ffe29
ValeriyaGatalskaya <stroller16012001@gmail.com>
2025-02-22 18:06:28 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,5 @@
+URL_SERVICE = 'https://df291dcf-2f86-42dc-b12d-24d6bfd87eea.serverhub.praktikum-services.ru/'
+
+CREATE_ORDER = "/api/v1/orders"
+
+PUT_ORDER = "/api/v1/orders/track"
---
+++ data.py
@@ -0,0 +1,17 @@
+order_body = {
+    "firstName": "Kak",
+    "lastName": "Nadoelo",
+    "address": "Moscow",
+    "metroStation": 4,
+    "phone": "+7 977 981 21 36",
+    "rentTime": 5,
+    "deliveryDate": "2025-23-02",
+    "comment": "Ya hochy spat",
+    "color": [
+        "BLACK"
+    ]
+}
+
+params_get = {
+    "t": ""
+}
