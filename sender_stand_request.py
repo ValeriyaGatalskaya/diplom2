1dbabbf7a7f4bee1b5a9d634ff1ac23dae59d575
ValeriyaGatalskaya <stroller16012001@gmail.com>
2025-02-23 17:58:49 +0300

    sender_stand_request.py

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -2,14 +2,12 @@ import configuration
 import requests
 import data
 
-
 def post_new_order(body):
     return requests.post(configuration.URL_SERVICE + configuration.CREATE_ORDER,
                          json=body)
 
-
-order_response = post_new_order(data.order_body)
-
+def get_new_track(body):
+    return post_new_order(body).json().get("track")  
 
 def get_order(track_order):
     return requests.get(configuration.URL_SERVICE + configuration.PUT_ORDER,
