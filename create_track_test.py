b66f3ea092af4161649d5ef8f9c528daa5ee110d
ValeriyaGatalskaya <stroller16012001@gmail.com>
2025-02-23 18:00:02 +0300

    create_track_test.py

--- create_track_test.py
+++ create_track_test.py
@@ -5,14 +5,11 @@ import data
 def get_new_track(order_response):
     return order_response.json().get("track")
 
-
-data.params_get["t"] = get_new_track(sender_stand_request.order_response)
-
-
-def positive_assert():
+def positive_assert(order_response):
+    data.params_get["t"] = get_new_track(order_response)  
     track_response = sender_stand_request.get_order(data.params_get)
     assert track_response.status_code == 200
 
-
 def test_order():
-    positive_assert()
+    order_response = sender_stand_request.post_new_order(data.body) 
+    positive_assert(order_response)
