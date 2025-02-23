d19fb9e6b832a48f928411818b615029ba3dcdcb
ValeriyaGatalskaya <stroller16012001@gmail.com>
2025-02-23 17:52:23 +0300

    onfiguration.py

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE = 'https://df291dcf-2f86-42dc-b12d-24d6bfd87eea.serverhub.praktikum-services.ru/'
+URL_SERVICE = 'https://df291dcf-2f86-42dc-b12d-24d6bfd87eea.serverhub.praktikum-services.ru'
 
 CREATE_ORDER = "/api/v1/orders"
 
