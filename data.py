50ef3fe75276eed81602c7485251f069db00093b
ValeriyaGatalskaya <stroller16012001@gmail.com>
2025-02-28 14:04:30 +0300

    data.py

--- data.py
+++ data.py
@@ -1,4 +1,3 @@
-# data.py
 order_body = {
     "firstName": "Kak",
     "lastName": "Nadoelo",
@@ -6,7 +5,7 @@ order_body = {
     "metroStation": 4,
     "phone": "+7 977 981 21 36",
     "rentTime": 5,
-    "deliveryDate": "2025-02-24",  # Изменила
+    "deliveryDate": "2025-02-29",  # Изменила
     "comment": "Ya hochy spat",
     "color": [
         "BLACK"
