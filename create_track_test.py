f88eab56802519e1dc7f8c97af8a65641baab2b0
ValeriyaGatalskaya <stroller16012001@gmail.com>
2025-02-28 14:02:37 +0300

    28.02.2025 Update create_track_test.py

--- create_track_test.py
+++ create_track_test.py
@@ -1,15 +1,13 @@
 # Гатальская Валерия, 26 когорта — Финальный проект. Инженер по тестированию плюс
+from sender_stand_request import get_new_track
 import sender_stand_request
 import data
 
-def get_new_track(order_response):
-    return order_response.json().get("track")
-
-def positive_assert(order_response):
-    data.params_get["t"] = get_new_track(order_response)
+def positive_assert(body):
+    data.params_get["t"] = get_new_track(body)
     track_response = sender_stand_request.get_order(data.params_get)
     assert track_response.status_code == 200
 
 def test_order():
-    order_response = sender_stand_request.post_new_order(data.order_body)  
-    positive_assert(order_response)
+    positive_assert(data.order_body)
+
